6728f1a81bf8995ef9d37ef12ca87454c27bbf88
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-10 14:56:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+
+// function superbowlWin(collection) {
+//    return collection.find((item)=>item.result === 'W' )
+    
+// }
+// console.log(superbowlWin(record))
+
+// function superbowlWin(collection) {
+//     let item = collection.find(item =>{item.result === 'W'});
+//     return item.year;
+    
+    
+// }
+
+const superbowlWin = (record) => {
+    for (let result of record) {
+      if (result.year) {
+        let myResult = record.find((item) => item.result === 'W');
+        return myResult.year;
+      } else {
+        return undefined;
+      }
+    }
+  }; 
+
+
