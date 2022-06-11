e41d600a0a530de9ebc4a607908fab9aa46ce38b
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-11 11:42:02 +0300

    commit tree 850927b6565ffad8b7967579d883f5546f40a40b

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 
-// function superbowlWin(collection) {
-//    return collection.find((item)=>item.result === 'W' )
-    
-// }
-// console.log(superbowlWin(record))
+// const superbowlWin = (record) => {
+//     let myResult = record.find((item) => item.result === 'W');
+//     if (myResult) {
+//       return myResult.year;
+//     } else {
+//       return undefined;
+//     }
+  
+// }; 
 
-// function superbowlWin(collection) {
-//     let item = collection.find(item =>{item.result === 'W'});
-//     return item.year;
-    
-    
-// }
 
-const superbowlWin = (record) => {
-    for (let result of record) {
-      if (result.year) {
-        let myResult = record.find((item) => item.result === 'W');
-        return myResult.year;
-      } else {
-        return undefined;
-      }
-    }
-  }; 
+
+
+
+
+
+
+
+
+
+
+
+
+const superbowlWin = (record)=>{
+  let myResult = record.find((item)=>item.result === 'W')
+  if (myResult) {
+    return myResult.year
+  } else {
+    return undefined
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
